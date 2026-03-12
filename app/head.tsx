3beaf2f1af42ef65275013769875b9a723c1ae34
Gregory Paul van Esch <greg@vanesch.uk>
2026-03-12 17:25:05 +0000

    Add canonical link to head for SEO

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,7 @@
 export default function Head() {
   return (
     <>
+      {/* Open Graph meta tags */}
       <meta
         property="og:image"
         content="https://www.vanesch.uk/og-image.jpg"
@@ -21,7 +22,11 @@ export default function Head() {
         content="Greg van Esch | HR Operations & Transformation Advisor"
       />
 
+      {/* Twitter meta tags */}
       <meta name="twitter:image" content="https://www.vanesch.uk/og-image.jpg" />
+      
+      {/* Canonical URL meta tag */}
+      <link rel="canonical" href="https://www.vanesch.uk" />
     </>
   );
 }
