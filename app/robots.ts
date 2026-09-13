a61f4bb6a332d54cd2c8a99a4815b1868a28e6de
Greg van Esch <greg@vanesch.uk>
2026-09-13 14:04:00 +0100

    Point robots to canonical www sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -13,6 +13,6 @@ export default function robots(): MetadataRoute.Robots {
         ],
       },
     ],
-    sitemap: "https://vanesch.uk/sitemap.xml",
+    sitemap: "https://www.vanesch.uk/sitemap.xml",
   };
 }
